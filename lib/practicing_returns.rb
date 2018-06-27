fed989589e5e0e27ce5ec86d492a468474209d11
Jordan Harris <jharris@wework.com>
2018-06-27 19:04:08 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -11,9 +11,6 @@ end
 binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
-# Notice that our method `puts`-ed out the code we expected and **returned `nil`**. Why is that?
-
-# That is because the return value of a `while` loop is always `nil`. If we want our method to return something else, we have to tell it to do so. 
 
 # Let's tell our method to return a new array of strings that contain the above greetings, instead of simply `puts`-ing out our greetings. 
 
