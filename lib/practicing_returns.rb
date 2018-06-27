f8cf460afc6b1ce88ca76bf87e1dc9fea4b98d3b
Jordan Harris <jharris@wework.com>
2018-06-27 19:03:31 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -11,48 +11,6 @@ end
 binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
-
-
-# In this method, we are using a `while` loop to iterate over an array and yielding each member of the array in turn to a block. 
-
-# We would call our method like this:
-
-# ```ruby
-# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
-# ```
-
-# Which would output to the terminal:
-
-# ```bash
-# # > Hi, Tim
-# # > Hi, Tom
-# # > Hi, Jim
-# ```
-
-# You can run this file in your terminal with `ruby lib/practicing_returns.rb` to see for yourself. 
-
-# What is the return value of this method call, though? 
-
-# Let's find out! Notice that we have required Pry for you at the top of `lib/practicing_returns.rb` Go ahead and place a `binding.pry` on line 11, just before we call our `#hello` method. 
-
-# Run the file with `ruby lib/practicing_returns.rb` in your terminal. We should be dropped right into our binding. 
-
-# Inside the Pry console in your terminal, manually invoke the method by copying and pasting the following and hitting `enter`:
-
-# ```
-# ruby
-# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
-# ```
-
-# You should see the following:
-
-# ```bash
-# # > Hi, Tim
-# # > Hi, Tom
-# # > Hi, Jim
-# => nil
-# ```
-
 # Notice that our method `puts`-ed out the code we expected and **returned `nil`**. Why is that?
 
 # That is because the return value of a `while` loop is always `nil`. If we want our method to return something else, we have to tell it to do so. 
